63fa81e804c0ecf7b3d52a1956fbde15f411c6e1
Valery <valkirik1995@mail.ru>
2025-05-17 18:04:04 +0300

    adding block content

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", title="Main")
+    return render_template("index.html", title="with title")
 
 if __name__ == "__main__":
     app.run(debug=True)
